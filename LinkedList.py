965bc52fce4d6df8fcc1c6ef4591f98039b1d89e
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2024-04-26 11:20:17 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -10,3 +10,22 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+        
+    #1 Print List
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+        #2 Append
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
