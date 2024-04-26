f6efe843c828635515ea794d3fb02b86dfdc2df3
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2024-04-26 11:20:47 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -29,3 +29,32 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
+
+     #3 Pop
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+        
+        #4 Prepend
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
