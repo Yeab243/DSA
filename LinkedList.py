e587b8bfba8c30a1ece00f4130b4cf6ece32495f
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2024-04-26 11:21:47 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -80,3 +80,26 @@ class LinkedList:
             temp = temp.next
         return temp
         
+         #7 Set Value
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+        #8 Insert
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1   
+        return True  
+        
