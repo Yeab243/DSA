0be542ce04abb8401d91714f482f15b9ebe4d666
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2024-04-26 11:22:49 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -130,4 +130,17 @@ class LinkedList:
             temp.next = before
             before = temp
             temp = after
+            my_linked_list = LinkedList(1)
+            
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+print('LL before reverse():')
+my_linked_list.print_list()
+
+my_linked_list.reverse()
+
+print('\nLL after reverse():')
+my_linked_list.print_list()
 
