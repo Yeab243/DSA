8e2638d4956a5ded2da518a82fa98904661ce4eb
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2024-04-26 11:21:19 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -58,3 +58,25 @@ class LinkedList:
             self.head = new_node
         self.length += 1
         return True
+
+        #5 Pop First
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+        #6 Get
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+        
