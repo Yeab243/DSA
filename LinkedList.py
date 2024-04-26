9280bd662b4c6c350b47208eb2fbab9ff847f3d6
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2024-04-26 11:22:17 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -103,3 +103,31 @@ class LinkedList:
         self.length += 1   
         return True  
         
+         #9 Remove
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+        
+        #10 Reverse
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
